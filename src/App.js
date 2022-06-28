32a3ca6438a331396f1dd06a787cfd18aeb5048a
serhatcigerci <yserhatcigerci@gmail.com>
2022-06-28 23:34:28 +0300

    Actions added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { useReducer } from "react"
 import "./styles.css"
 
-function reducer(state, action) {
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
 
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand}${payload.digit}`
+      }
+  }
 }
 
 function App() {
