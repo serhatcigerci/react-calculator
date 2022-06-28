6ab9def22f9d58a95fa06e55cc2c5e3f20ea8b9c
serhatcigerci <yserhatcigerci@gmail.com>
2022-06-28 23:00:02 +0300

    Operands and useReducer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useReducer } from "react"
 import "./styles.css"
 
+function reducer(state, action) {
+
+}
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, 
+    dispatch] = useReducer(reducer, {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123213123123</div>
-        <div className="current-operand">123123</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
